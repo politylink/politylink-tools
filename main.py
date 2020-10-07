979886566652d71f3d56431c036d8c4935be642b
MitsukiUsui <mitsuo.com.mu@gmail.com>
2020-10-07 22:45:21 +0900

    POL-130: fix main.py

--- main.py
+++ main.py
@@ -22,28 +22,29 @@ def fetch_all_news(gql_client):
     }
     """
     res = gql_client.exec(query)
-    return res["data"]["News"]
+    return res["News"]
+
+
+def call_api(news, news_text, handler):
+    text = " ".join([news_text.title, news_text.body])
+    date = news["publishedAt"]
+    date_str = "{}/{}/{} {}:{}".format(date["year"], date["month"], date["day"], date["hour"], date["minute"])
+    json_data = json.dumps({"text": text, "date": date_str}, ensure_ascii=False)
+    res = requests.post(handler,
+                        data=json_data.encode("utf-8"),
+                        headers={'Content-Type': 'application/json'})
+    return res.json()
 
 
 def fetch_matched_minutes(news, news_text):
-    text = " ".join([news_text.title, news_text.body])
-    date = news["publishedAt"]
-    date_str = "{}/{}/{} {}:{}".format(date["year"], date["month"], date["day"], date["hour"], date["minute"])
-    json_data = json.dumps({"text": text, "date": date_str}, ensure_ascii=False)
-    res = requests.post(MINUTES_HANDLER,
-                        data=json_data.encode("utf-8"),
-                        headers={'Content-Type': 'application/json'})
-    return res.json()['minutes']
+    res = call_api(news, news_text, MINUTES_HANDLER)
+    return res['minutes']
+
 
 def fetch_matched_bills(news, news_text):
-    text = " ".join([news_text.title, news_text.body])
-    date = news["publishedAt"]
-    date_str = "{}/{}/{} {}:{}".format(date["year"], date["month"], date["day"], date["hour"], date["minute"])
-    json_data = json.dumps({"text": text, "date": date_str}, ensure_ascii=False)
-    res = requests.post(BILLS_HANDLER,
-                        data=json_data.encode("utf-8"),
-                        headers={'Content-Type': 'application/json'})
-    return res.json()['bills']
+    res = call_api(news, news_text, BILLS_HANDLER)
+    return res['bills']
+
 
 def main():
     gql_client = GraphQLClient()
@@ -62,12 +63,12 @@ def main():
         if minutes_list:
             LOGGER.info(f'found {len(minutes_list)} minutes for {news["id"]}')
             for minutes in minutes_list:
-                gql_client.exec_merge_news_referred_minutes(news["id"], minutes["id"])
-        bills_list = fetch_matched_bills(news, news_text)
-        if bills_list:
-            LOGGER.info(f'found {len(bills_list)} bills for {news["id"]}')
-            for bill in bills_list:
-                gql_client.exec_merge_news_referred_bills(news["id"], bill["id"])
+                gql_client.link(news["id"], minutes["id"])
+        bill_list = fetch_matched_bills(news, news_text)
+        if bill_list:
+            LOGGER.info(f'found {len(bill_list)} bills for {news["id"]}')
+            for bill in bill_list:
+                gql_client.link(news["id"], bill["id"])
 
 
 if __name__ == '__main__':
