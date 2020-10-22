b6a0ffb92723fc63eb9b37d8effcabe41e47fe81
MitsukiUsui <mitsuo.com.mu@gmail.com>
2020-10-23 00:58:18 +0900

    POL-110: add shell scripts for cron

---
+++ cron_daily.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# 0 0 * * * ~/politylink/politylink-tools/cron_daily.sh &> ~/politylink/politylink-tools/log/cron_daily.log
+
+set -ue
+
+cd ~/politylink/politylink-tools
+logrotate logrotate.conf -s /tmp/logrotate.state
+poetry run python cron.py -m daily
