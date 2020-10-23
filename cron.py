e96f9e5b41d40344305fe7b2564718bf62b2b8d0
MitsukiUsui <mitsuo.com.mu@gmail.com>
2020-10-23 14:32:16 +0900

    POL-110: separate daily and hourly log

--- cron.py
+++ cron.py
@@ -14,7 +14,7 @@ TOOLS_ROOT = POLITYLINK_ROOT / 'politylink-tools'
 LOG_ROOT = TOOLS_ROOT / 'log'
 
 TODAY = datetime.now().date()
-YESTERDAY = TODAY - timedelta(1)
+TOMMOROW = TODAY + timedelta(1)
 SEVEN_DAYS_AGO = TODAY - timedelta(7)
 DATE_FORMAT = '%Y-%m-%d'
 
@@ -26,7 +26,7 @@ class BashTask:
     def __init__(self, cmd, cwd=None, log_fp=None):
         self.cmd = cmd
         self.cwd = cwd if cwd else '.'
-        self.log_fp = LOG_ROOT / log_fp if log_fp else '/dev/null'
+        self.log_fp = str(log_fp) if log_fp else '/dev/null'
 
     def __repr__(self):
         return f'<BashTask {self.cmd}>'
@@ -36,34 +36,45 @@ class BashTask:
             return subprocess.run(self.cmd, shell=True, cwd=self.cwd, stdout=f, stderr=f, encoding='utf-8')
 
 
+DAILY_LOG_ROOT = LOG_ROOT / 'daily'
 DAILY_TASKS = [
-    BashTask('poetry run scrapy crawl shugiin', CRAWLER_ROOT, 'crawl_shugiin.log'),
-    BashTask('poetry run scrapy crawl sangiin', CRAWLER_ROOT, 'crawl_sangiin.log'),
-    BashTask('poetry run scrapy crawl shugiin_committee', CRAWLER_ROOT, 'crawl_shugiin_committee.log'),
-    BashTask('poetry run scrapy crawl sangiin_committee', CRAWLER_ROOT, 'crawl_sangiin_committee.log'),
+    BashTask('poetry run scrapy crawl shugiin',
+             CRAWLER_ROOT, DAILY_LOG_ROOT / 'crawl_shugiin.log'),
+    BashTask('poetry run scrapy crawl sangiin',
+             CRAWLER_ROOT, DAILY_LOG_ROOT / 'crawl_sangiin.log'),
+    BashTask('poetry run scrapy crawl shugiin_committee',
+             CRAWLER_ROOT, DAILY_LOG_ROOT / 'crawl_shugiin_committee.log'),
+    BashTask('poetry run scrapy crawl sangiin_committee',
+             CRAWLER_ROOT, DAILY_LOG_ROOT / 'crawl_sangiin_committee.log'),
     BashTask('poetry run scrapy crawl minutes -a start_date={} -a end_date={} -a speech=false'.format(
-        SEVEN_DAYS_AGO.strftime(DATE_FORMAT), TODAY.strftime(DATE_FORMAT)),
-        CRAWLER_ROOT, 'crawl_minutes.log'),
+        SEVEN_DAYS_AGO.strftime(DATE_FORMAT), TOMMOROW.strftime(DATE_FORMAT)),
+        CRAWLER_ROOT, DAILY_LOG_ROOT / 'crawl_minutes.log'),
     BashTask('poetry run python news.py --start_date {} --end_date {}'.format(
-        SEVEN_DAYS_AGO.strftime(DATE_FRMAT), TODAY.strftime(DATE_FORMAT)),
-        TOOLS_ROOT, 'process_news_daily.log'),
+        SEVEN_DAYS_AGO.strftime(DATE_FORMAT), TOMMOROW.strftime(DATE_FORMAT)),
+        TOOLS_ROOT, DAILY_LOG_ROOT / 'process_news.log'),
     BashTask('poetry run python timeline.py --start_date {} --end_date {}'.format(
-        SEVEN_DAYS_AGO.strftime(DATE_FORMAT), TODAY.strftime(DATE_FORMAT)),
-        TOOLS_ROOT, 'process_timeline_daily.log'),
+        SEVEN_DAYS_AGO.strftime(DATE_FORMAT), TOMMOROW.strftime(DATE_FORMAT)),
+        TOOLS_ROOT, DAILY_LOG_ROOT / 'process_timeline.log'),
 ]
 
+HOURLY_LOG_ROOT = LOG_ROOT / 'hourly'
 HOURLY_TASKS = [
-    BashTask('poetry run scrapy crawl reuters -a limit=50', CRAWLER_ROOT, 'crawl_reuters.log'),
-    BashTask('poetry run scrapy crawl reuters_kyodo -a limit=50', CRAWLER_ROOT, 'crawl_reuters_kyodo.log'),
-    BashTask('poetry run scrapy crawl nikkei -a limit=50', CRAWLER_ROOT, 'crawl_nikkei.log'),
-    BashTask('poetry run scrapy crawl mainichi -a limit=50', CRAWLER_ROOT, 'crawl_mainichi.log'),
+    BashTask('poetry run scrapy crawl reuters -a limit=50',
+             CRAWLER_ROOT, HOURLY_LOG_ROOT / 'crawl_reuters.log'),
+    BashTask('poetry run scrapy crawl reuters_kyodo -a limit=50',
+             CRAWLER_ROOT, HOURLY_LOG_ROOT / 'crawl_reuters_kyodo.log'),
+    BashTask('poetry run scrapy crawl nikkei -a limit=50',
+             CRAWLER_ROOT, HOURLY_LOG_ROOT / 'crawl_nikkei.log'),
+    BashTask('poetry run scrapy crawl mainichi -a limit=50',
+             CRAWLER_ROOT, HOURLY_LOG_ROOT / 'crawl_mainichi.log'),
     BashTask('poetry run python news.py --start_date {} --end_date {}'.format(
-        YESTERDAY.strftime(DATE_FORMAT), TODAY.strftime(DATE_FORMAT)),
-        TOOLS_ROOT, 'process_news.log'),
+        TODAY.strftime(DATE_FORMAT), TOMMOROW.strftime(DATE_FORMAT)),
+        TOOLS_ROOT, HOURLY_LOG_ROOT / 'process_news.log'),
     BashTask('poetry run python timeline.py --start_date {} --end_date {}'.format(
-        YESTERDAY.strftime(DATE_FORMAT), TODAY.strftime(DATE_FORMAT)),
-        TOOLS_ROOT, 'process_timeline.log'),
-    BashTask('gatsby clean && gatsby build', GATSBY_ROOT, 'gatsby.log'),
+        TODAY.strftime(DATE_FORMAT), TOMMOROW.strftime(DATE_FORMAT)),
+        TOOLS_ROOT, HOURLY_LOG_ROOT / 'process_timeline.log'),
+    BashTask('gatsby clean && gatsby build',
+             GATSBY_ROOT, HOURLY_LOG_ROOT / 'gatsby.log'),
 ]
 
 
@@ -99,5 +110,6 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--mode', type=Mode, choices=list(Mode), required=True)
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
-    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO, datefmt=LOG_DATE_FORMAT, format=LOG_FORMAT)
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO,
+                        datefmt=LOG_DATE_FORMAT, format=LOG_FORMAT)
     main(args.mode)
