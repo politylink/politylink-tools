31eb1332007ac6c9e7de02d66d23541b6f3c15d5
lwhb <lawhub.tokyo@gmail.com>
2020-10-23 16:16:12 +0900

    POL-110: fix logrotate config

--- cron_daily.sh
+++ cron_daily.sh
@@ -1,10 +1,10 @@
 #!/bin/bash --login
 
 # crontab -e
-# 0 0 * * * ~/politylink/politylink-tools/cron_daily.sh &> ~/politylink/politylink-tools/log/cron_daily.log
+# 0 0 * * * ~/politylink/politylink-tools/cron_daily.sh &> ~/politylink/politylink-tools/log/daily/cron.log
 
 set -ue
 
 cd ~/politylink/politylink-tools
-logrotate logrotate.conf -s /tmp/logrotate.state
+logrotate ./data/logrotate_daily.conf -s /tmp/logrotate.state
 poetry run python cron.py -m daily
--- cron_hourly.sh
+++ cron_hourly.sh
@@ -1,10 +1,10 @@
 #!/bin/bash --login
 
 # crontab -e
-# 0 * * * * ~/politylink/politylink-tools/cron_hourly.sh &> ~/politylink/politylink-tools/log/cron_hourly.log
+# 0 * * * * ~/politylink/politylink-tools/cron_hourly.sh &> ~/politylink/politylink-tools/log/hourly/cron.log
 
 set -ue
 
 cd ~/politylink/politylink-tools
-logrotate logrotate.conf -s /tmp/logrotate.state
+logrotate ./data/logrotate_hourly.conf -s /tmp/logrotate.state
 poetry run python cron.py -m hourly
