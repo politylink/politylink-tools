74864d7059a3dc7587530b6d3b3dd37931c5baae
MitsukiUsui <MitsukiUsui@users.noreply.github.com>
2021-01-09 20:56:42 +0900

    POL-181: add member social links (#16)
    * POL-181: add member social links
    
    * POL-181: check member.name_hira
    
    * POL-181: fix argparse desc
    
    * POL-181: rename file
    
    * POL-181: rename file

---
+++ fetch_member_links.py
@@ -0,0 +1,52 @@
+import argparse
+import logging
+
+import pandas as pd
+import requests
+from scrapy.http import HtmlResponse
+
+LOGGER = logging.getLogger(__name__)
+
+
+def scrape_table(table):
+    records = []
+    for row in table.xpath('./tr')[1:]:
+        cells = row.xpath('./td')
+        record = {
+            'name': ''.join(cells[4].xpath('.//text()').get().split()),
+            'name_hira': ''.join(cells[4].xpath('./a/span[@class="ruby"]/text()').get().split()),
+            'website': row.xpath('.//a[@class="links weblink"]/@href').get(),
+            'twitter': row.xpath('.//a[@class="links twitters"]/@href').get(),
+            'facebook': row.xpath('.//a[@class="links facebooks"]/@href').get()
+        }
+        records.append(record)
+    LOGGER.debug(f'scraped {len(records)} records from table')
+    return records
+
+
+def main(fp):
+    records = []
+
+    url = 'https://democracy.minibird.jp/'
+    response = HtmlResponse(url=url, body=requests.get(url).text, encoding='utf-8')
+    table = response.xpath('//table[@id="table1"]')
+    records += scrape_table(table)
+
+    url = 'https://democracy.minibird.jp/councillors.php'
+    response = HtmlResponse(url=url, body=requests.get(url).text, encoding='utf-8')
+    table = response.xpath('//table[@id="table2"]')
+    records += scrape_table(table)
+
+    df = pd.DataFrame(records)
+    df.to_csv(fp, index=False)
+    LOGGER.info(f'saved {len(records)} records to {fp}')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MemberのリンクをCSVに保存する')
+    parser.add_argument('-f', '--file', default='./data/member_links.csv')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+    logging.getLogger('sgqlc').setLevel(logging.INFO)
+    main(args.file)
---
+++ merge_member_links.py
@@ -0,0 +1,46 @@
+import argparse
+import logging
+
+import pandas as pd
+
+from politylink.graphql.client import GraphQLClient
+from politylink.helpers import MemberFinder
+
+LOGGER = logging.getLogger(__name__)
+
+
+def main(fp):
+    client = GraphQLClient()
+    member_finder = MemberFinder(search_fields=['name', 'name_hira'])
+
+    df = pd.read_csv(fp).fillna('')
+    LOGGER.info(f'load {len(df)} members from {fp}')
+
+    members = []
+    for _, row in df.iterrows():
+        member = None
+        for search_field in ['name', 'name_hira']:
+            try:
+                member = member_finder.find_one(row[search_field], exact_match=True)
+                break
+            except ValueError as e:
+                LOGGER.debug(e)
+        if not member:
+            LOGGER.warning(f'failed to find member for row={row}')
+            continue
+        for link_field in ['website', 'twitter', 'facebook']:
+            if row[link_field]:
+                setattr(member, link_field, row[link_field])
+        members.append(member)
+    client.bulk_merge(members)
+    LOGGER.info(f'merged {len(members)} member links')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='MemberのリンクをCSVからGraphQLに追加する')
+    parser.add_argument('-f', '--file', default='./data/member_links.csv')
+    parser.add_argument('-v', '--verbose', action='store_true')
+    args = parser.parse_args()
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+    logging.getLogger('sgqlc').setLevel(logging.INFO)
+    main(args.file)
