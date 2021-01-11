5144759ac625b2e37a9bc6c1f30dba32a9fa8b80
kenya-sk <ksakka0309@gmail.com>
2021-01-11 23:50:07 +0900

    POL-184: reflect review

--- transcribe_voice.sh
+++ transcribe_voice.sh
@@ -3,17 +3,17 @@ set -ex
 FILE_NAME="video_1"
 
 VOICE_URL="https://webtv-vod.live.ipcasting.jp/vod/mp4:5992.mp4/playlist.m3u8"
-OUTPUT_FILE_MP4="${FILE_NAME}.mp4"
+OUTPUT_FILE_MP4="./data/${FILE_NAME}.mp4"
 ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "${VOICE_URL}" -c copy "${OUTPUT_FILE_MP4}"
 
-OUTPUT_FILE_WAV="${FILE_NAME}.wav"
+OUTPUT_FILE_WAV="./data/${FILE_NAME}.wav"
 ffmpeg -i "${OUTPUT_FILE_MP4}" "${OUTPUT_FILE_WAV}"
 
 BUCKET="politylink-speech"
 GCS_VOICE_PATH="gs://${BUCKET}/voice/${OUTPUT_FILE_WAV}"
 gsutil cp "${OUTPUT_FILE_WAV}" "${GCS_VOICE_PATH}"
 
-SAVE_PATH="../data/${FILE_NAME}.json"
+SAVE_PATH="./data/${FILE_NAME}.json"
 python transcribe_voice.py "${OUTPUT_FILE_WAV}" "${GCS_VOICE_PATH}" "${SAVE_PATH}"
 
 GCS_TRANS_PATH="gs://${BUCKET}/transcription/${FILE_NAME}.json"
