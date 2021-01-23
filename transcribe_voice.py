f576b68c47fc3ccdaddcd53403807f74359362fe
kenya-sk <ksakka0309@gmail.com>
2021-01-23 17:35:27 +0900

    POL-184: transcribe voice data (#17)
    * POL-184: transcribe voice data
    
    * POL-184: add packate
    
    * POL-184: reflect review
    
    * POL-184: reflect review
    
    * POL-184: reflect review
    
    * POL-184: modify parameter
    
    * POL-184: update transcribe_voice.sh path
    
    * POL-184: insert punctuation in transcription
    
    Co-authored-by: MitsukiUsui <mitsuo.com.mu@gmail.com>

---
+++ transcribe_voice.py
@@ -0,0 +1,113 @@
+import json
+import os
+import re
+import sys
+import wave
+
+from google.cloud import speech
+
+
+def insert_punctuation(text):
+    inserted_text = re.sub(r'ました(。)?', 'ました。', text)
+    inserted_text = re.sub(r'します(。)?', 'します。', inserted_text)
+    inserted_text = re.sub(r'きます(。)?', 'きます。', inserted_text)
+    inserted_text = re.sub(r'います(。)?', 'います。', inserted_text)
+
+    return inserted_text
+
+
+def format_transcription(transcription_json):
+    formatted_texts = ''
+    for i in range(len(transcription_json)):
+        text = transcription_json[f'speech{i}']['transcript']
+        inserted_text = insert_punctuation(text)
+        formatted_texts += inserted_text
+
+    return formatted_texts
+
+
+def get_voice_info(local_file_path):
+    if os.path.isfile(local_file_path):
+        wr = wave.open(local_file_path, 'rb')
+        channels = wr.getnchannels()
+        rate = wr.getframerate()
+        frame_num = wr.getnframes()
+        length = 1.0 * frame_num / rate
+    else:
+        print(f'Error: "{local_file_path}" does not exist.')
+        sys.exit()
+
+    print('\n-*- audio info -*-')
+    print(f'filename   : {local_file_path}')
+    print(f'sampleRate : {str(rate)}')
+    print(f'playtime   : {str(length)} [sec]')
+    print(f'channels   : {str(channels)}')
+    print('\nWaiting for operation to complete...')
+
+    return rate, length, channels
+
+
+def save_transcription(response, save_path):
+    transcription_json = {}
+    for index, item in enumerate(response.results):
+        transcription_json[f'speech{index}'] = {
+            'confidence': item.alternatives[0].confidence,
+            'transcript': item.alternatives[0].transcript
+        }
+
+    # Raw transcription data
+    with open(save_path, 'w') as f:
+        json.dump(
+            transcription_json, f, indent=4, ensure_ascii=False)
+    print(f'Raw data is Saved in {save_path}')
+
+    # Formatted transcription data
+    # save_path extension converts '.json' to '.txt'
+    formatted_texts = format_transcription(transcription_json)
+    print(formatted_texts)
+    save_text_path = os.path.splitext(save_path)[0] + '.txt'
+    with open(save_text_path, 'w') as f:
+        f.write(formatted_texts)
+    print(f'Formatted data is Saved in {save_text_path}')
+
+
+def transcribe_voice(local_file_path, gcs_file_path, save_path):
+    # fetch voice content
+    rate, length, channels = get_voice_info(local_file_path)
+
+    # cloud cost
+    print(f'The cost of the cloud is around ${0.008*length/15:.2f}')
+
+    # set config of GCP speech-to-text
+    config = {
+        'encoding': 'LINEAR16',
+        'sample_rate_hertz': rate,
+        'language_code': 'ja-JP',
+        'audio_channel_count': channels,
+        'enable_automatic_punctuation': True,
+        'diarization_config': {
+            "enable_speaker_diarization": True,
+            "min_speaker_count": 1,
+            "max_speaker_count": 10}
+    }
+
+    # set GCS URI of voice data
+    audio = {'uri': gcs_file_path}
+
+    # transcribe voice data
+    client = speech.SpeechClient()
+    operation = client.long_running_recognize(config=config, audio=audio)
+    print(f'operation name = {operation.operation.name}')
+    response = operation.result(timeout=length)
+    print('\n-*- transcribe result -*-')
+
+    # save transcribed text data
+    save_transcription(response, save_path)
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) == 4:
+        transcribe_voice(args[1], args[2], args[3])
+    else:
+        sys.exit('Error: invalid argument')
---
+++ transcribe_voice.sh
@@ -0,0 +1,20 @@
+set -ex
+
+# Need to set the environment variable "GOOGLE_APPLICATION_CREDENTIALS".
+
+FILE_NAME="sangiin_20210121"
+VIDEO_URL="https://webtv-vod.live.ipcasting.jp/vod/mp4:6109.mp4/playlist.m3u8"
+
+LOCAL_VIDEO_PATH="./voice/${FILE_NAME}.mp4"
+LOCAL_VOICE_PATH="./voice/${FILE_NAME}.wav"
+LOCAL_TRANS_PATH="./voice/${FILE_NAME}.json"
+
+GCS_BUCKET="politylink-speech-mu"
+GCS_VOICE_PATH="gs://${GCS_BUCKET}/voice/${FILE_NAME}.wav"
+GCS_TRANS_PATH="gs://${GCS_BUCKET}/transcription/${FILE_NAME}.json"
+
+ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "${VIDEO_URL}" -c copy "${LOCAL_VIDEO_PATH}"
+ffmpeg -i "${LOCAL_VIDEO_PATH}" "${LOCAL_VOICE_PATH}"
+gsutil cp "${LOCAL_VOICE_PATH}" "${GCS_VOICE_PATH}"
+python transcribe_voice.py "${LOCAL_VOICE_PATH}" "${GCS_VOICE_PATH}" "${LOCAL_TRANS_PATH}"
+gsutil cp "${LOCAL_TRANS_PATH}" "${GCS_TRANS_PATH}"
