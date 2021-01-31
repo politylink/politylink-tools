d5bafda0e2313e46cc027c48da7924008c787c2e
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-01-31 23:42:38 +0900

    POL-228: add publish option in fetch_transcription_results.py

--- fetch_transcription_results.py
+++ fetch_transcription_results.py
@@ -3,8 +3,13 @@ import json
 import logging
 import os
 
+import boto3
 import requests
+from jinja2 import Environment, FileSystemLoader
 
+from politylink.graphql.client import GraphQLClient
+from politylink.graphql.schema import Url
+from politylink.idgen import idgen
 from transcribe_voice import insert_punctuation
 
 LOGGER = logging.getLogger(__name__)
@@ -37,7 +42,7 @@ class SpeechRestClient:
         response = requests.get(url)
         data = response.json()
 
-        if not data['done']:
+        if ('done' not in data) or (not data['done']):
             raise ValueError('operation is not finished')
 
         if 'error' in data:
@@ -77,33 +82,78 @@ def format_transcripts(data):
     return transcripts
 
 
+def build_html(template, data, minutes):
+    date = minutes.start_date_time
+    html = template.render({
+        'minutes': minutes.name,
+        'date': f'{date.year}-{date.month}-{date.day}',
+        'url': 'https://politylink.jp/minutes/{}'.format(minutes.id.split(':')[-1]),
+        'transcripts': format_transcripts(data)
+    })
+    return html
+
+
+def build_url(s3_url):
+    url = Url({
+        'url': s3_url,
+        'title': '自動文字起こし',
+        'domain': 'politylink.jp'
+    })
+    url.id = idgen(url)
+    return url
+
+
 def main():
-    client = SpeechRestClient()
-    ids = client.list_()
-    LOGGER.info(f'found total {len(ids)} operation ids: {ids}')
-    for id_ in ids:
+    graphql_client = GraphQLClient()
+    speech_client = SpeechRestClient()
+    s3_client = boto3.client('s3')
+    html_template = Environment(loader=FileSystemLoader('./data', encoding='utf8')) \
+        .get_template('transcription_template.html')
+
+    operation_ids = speech_client.list_()
+    LOGGER.info(f'found total {len(operation_ids)} operation ids: {operation_ids}')
+
+    for operation_id in operation_ids:
         try:
-            data = client.get(id_)
+            data = speech_client.get(operation_id)
         except Exception as e:
-            LOGGER.warning(f'failed to fetch operation result for {id_}: {e}')
+            LOGGER.warning(f'failed to fetch operation result for {operation_id}: {e}')
             continue
 
-        json_fp = './voice/{}.json'.format(data['metadata']['job_name'])
-        text_fp = json_fp.replace('.json', '.txt')
+        job_name = data['metadata']['job_name']
+        minutes_id = f'Minutes:{job_name}'
+
+        try:
+            minutes = graphql_client.get(minutes_id)
+        except Exception as e:
+            LOGGER.warning(f'failed to get minutes: {e}')
+            continue
+
+        json_fp = f'./voice/{job_name}.json'
+        html_fp = f'./voice/{job_name}.html'
+        s3_fp = f'minutes/{job_name}.html'
+        s3_url = f'https://text.politylink.jp/{s3_fp}'
 
         with open(json_fp, 'w') as f:
             json.dump(data, f, ensure_ascii=False)
-        LOGGER.info(f'saved raw result in {json_fp}')
+        LOGGER.info(f'saved JSON result in {json_fp}')
 
-        transcripts = format_transcripts(data)
-        with open(text_fp, 'w') as f:
-            for text in transcripts:
-                f.write(f'{text}\n\n')
-        LOGGER.info(f'saved text result in {text_fp}')
+        html = build_html(html_template, data, minutes)
+        with open(html_fp, 'w', encoding="utf-8") as f:
+            f.write(html)
+        LOGGER.info(f'saved HTML in {html_fp}')
+
+        if args.publish:
+            s3_client.upload_file(html_fp, 'politylink-text', s3_fp, ExtraArgs={"ContentType": "text/html"})
+            url = build_url(s3_url)
+            graphql_client.merge(url)
+            graphql_client.link(url.id, minutes.id)
+            LOGGER.info(f'published HTML to S3: {s3_fp}')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='GCPの文字起こしAPIの結果をRESTで取得する')
+    parser.add_argument('-p', '--publish', help='S3にtextファイルを送る', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
 
