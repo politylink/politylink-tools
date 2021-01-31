a627450deb0da19d0715e50c1a7bb16a624b5d94
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-01-31 20:44:05 +0900

    POL-230: use pydum.utils.mediainfo to get audio info

--- transcribe_voice.sh
+++ transcribe_voice.sh
@@ -12,6 +12,6 @@ GCS_BUCKET="politylink-speech-mu"
 GCS_VOICE_PATH="gs://${GCS_BUCKET}/voice/${JOB_NAME}.mp3"
 
 yes | ffmpeg -protocol_whitelist file,http,https,tcp,tls,crypto -i "${VIDEO_URL}" -c copy "${LOCAL_VIDEO_PATH}"
-yes | ffmpeg -i "${LOCAL_VIDEO_PATH}" "${LOCAL_VOICE_PATH}"
+yes | ffmpeg -i "${LOCAL_VIDEO_PATH}" -ar 44100 -ac 1 "${LOCAL_VOICE_PATH}"
 yes | gsutil cp "${LOCAL_VOICE_PATH}" "${GCS_VOICE_PATH}"
 yes | poetry run python transcribe_voice.py --local "${LOCAL_VOICE_PATH}" --gcs "${GCS_VOICE_PATH}"
