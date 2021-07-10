ce30a83f4bb33b3a333c0cf5e785d5ead96c651d
MitsukiUsui <mitsuo.com.mu@gmail.com>
2021-07-10 14:19:21 +0900

    POL-328: incremental bill_thumbnail

--- bill_thumbnail.py
+++ bill_thumbnail.py
@@ -1,11 +1,11 @@
 import argparse
 import logging
+from collections import defaultdict
+from pathlib import Path
 
 import boto3
-import os
 import requests
 import time
-from collections import defaultdict
 from tqdm import tqdm
 from wand.image import Image as wandImage
 
@@ -14,56 +14,91 @@ from politylink.graphql.client import GraphQLClient
 LOGGER = logging.getLogger(__name__)
 
 """
-Refer: https://qiita.com/tomtsutom0122/items/bb5acaee4f4f7820124c
-
 requires ~/.aws/credentials
 https://boto3.amazonaws.com/v1/documentation/api/latest/guide/quickstart.html
 """
 
-def save_thumbnail(data, local_path):
+
+def save_image(data, local_path):
+    """
+    https://qiita.com/tomtsutom0122/items/bb5acaee4f4f7820124c
+    """
+
     image = wandImage(blob=data, resolution=300, format="pdf")
     if len(image.sequence) > 1:
         # Select the first page of the pdf
         image = wandImage(image.sequence[0])
     image.transform(resize='640')
-    image.crop(width=640, height=360)
+    image.crop(width=640, height=320)
     image.alpha_channel = 'remove'
+    local_path.parent.mkdir(parents=True, exist_ok=True)
     image.save(filename=local_path)
     return image.make_blob()
 
-def main():
-    client = GraphQLClient(url="https://graphql.politylink.jp/")
-    bills = client.get_all_bills(fields=['id', 'urls'])
-    stats = defaultdict(int)
-    s3_client = boto3.client('s3')
-    os.makedirs("./image/bill", exist_ok=True)
 
+def get_maybe_summary_pdf(bill):
+    for url in bill.urls:
+        if url.title == '概要PDF':
+            return url.url.replace('http://', 'https://')
+    return None
+
+
+def main():
+    gql_client = GraphQLClient(url="https://graphql.politylink.jp/")
+    s3_client = boto3.client('s3')
+
+    bills = gql_client.get_all_bills(fields=['id', 'urls'])
+    LOGGER.info(f'fetched {len(bills)} bills')
+
+    stats = defaultdict(int)
     for bill in tqdm(bills):
-        summary_pdf = next(filter(lambda x: x.title == "概要PDF", bill.urls), None)
-        LOGGER.debug(f'Processing ... {bill.id}')
-        if summary_pdf:
-            stats['process'] += 1
-            id_ = bill.id.split(':')[-1]
-            local_path = f'./image/bill/{id_}.png'
-            try:
-                response = requests.get(summary_pdf.url)
-                thumbnail = save_thumbnail(response, local_path)
-            except Exception as e:
-                LOGGER.warning(f'failed to convert summary pdf to png: {e}')
-                stats['fail'] += 1
-                continue
-            if args.publish:
-                s3_path = f'bill/{id_}.png'
-                s3_client.upload_file(local_path, 'politylink', s3_path, ExtraArgs={'ContentType': 'image/png'})
-            time.sleep(1)
+        LOGGER.debug(f'check {bill.id}')
+
+        id_body = bill.id.split(':')[-1]
+        local_path = Path(f'./image/bill/{id_body}.png')
+        s3_path = Path(f'bill/{id_body}.png')
+
+        if local_path.exists() and not args.overwrite:
+            LOGGER.debug(f'{local_path} already exists')
+            continue
+
+        maybe_summary_pdf = get_maybe_summary_pdf(bill)
+        if not maybe_summary_pdf:
+            LOGGER.debug('summary PDF does not exist')
+            continue
+        summary_pdf = maybe_summary_pdf
+
+        stats['process'] += 1
+        time.sleep(1)
+
+        response = requests.get(summary_pdf)
+        if not response.ok:
+            LOGGER.warning(f'failed to fetch {summary_pdf}')
+            stats['fail'] += 1
+            continue
+
+        try:
+            save_image(response, local_path)
+        except Exception:
+            LOGGER.exception(f'failed to save {summary_pdf}')
+            stats['fail'] += 1
+            continue
+        finally:
+            LOGGER.debug(f'saved {local_path}')
+
+        if args.publish:
+            s3_client.upload_file(local_path, 'politylink', s3_path, ExtraArgs={'ContentType': 'image/png'})
+            LOGGER.debug(f'published {s3_path}')
+
     LOGGER.info('processed {} bills ({} success, {} fail)'.format(
         stats['process'], stats['process'] - stats['fail'], stats['fail']
     ))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='法律案概要pdfのサムネイルをS3にアップロードする')
+    parser = argparse.ArgumentParser(description='法律案のサムネイルを概要PDFから生成する')
     parser.add_argument('-p', '--publish', help='画像をS3にアップロードする', action='store_true')
+    parser.add_argument('-o', '--overwrite', help='画像を再生成する', action='store_true')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
 
